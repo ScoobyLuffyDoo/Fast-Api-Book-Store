6a36d33116e015d77b1d24f789114b90b5d2050d
ScoobyLuffyDoo <ScoobyLuffyDoo@gmail.com>
2022-02-09 00:20:37 +0200

    BookDetails Crud Service

---
+++ BookDetailsService.py
@@ -0,0 +1,28 @@
+import sqlite3
+import json
+
+
+
+class bookDetails_SRV:    
+    def createBookDetails(self,bookDetials):
+        return tst
+    def readBookDetails(self):
+        return tst
+    def updateBookDetails(self):
+        return tst    
+    def deleteBookDetails(self):
+        return tst
+
+
+
+
+
+
+# connection = sqlite3.connect(path)
+#     cursor = connection.cursor()
+#     sql = "SELECT * FROM BooksTable"
+#     cursor.execute(sql)
+#     output = cursor.fetchmany(5)
+#     connection.close()
+#     tst =json.dumps(output)
+#     print(output)
--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from typing  import Optional
 from fastapi import FastAPI
-import sqlite3
+
 path='BookStore_DB.db'
 
 
@@ -13,18 +13,11 @@ def Blha():
 
 @app.get('/booksdetails') 
 def get_bookDetails():
-    connection = sqlite3.connect(path)
-    cursor = connection.cursor()
-    sql = "SELECT * FROM BooksTable"
-    cursor.execute(sql)
-    output = cursor.fetchmany(5)
-    connection.close()
+
     return(output)
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.post('/bookdetail/bookinfo')
+def create_BookRecord(TST:bookinfo):
+    print(TST)
 
-
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    print(type(fake_items_db))
-    return fake_items_db[skip : skip + limit]
+# uvicorn index:app --reload
